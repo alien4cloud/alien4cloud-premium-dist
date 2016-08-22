28de01de29bcc9ad3ab499ca3ebb2c319e85a810
Luc Boutier <luc@fastconnect.fr>
2016-08-22 10:05:56 +0200

    Fix premium dist packaging

--- src/main/assembly/scripts/alien4cloud.sh
+++ src/main/assembly/scripts/alien4cloud.sh
@@ -24,6 +24,6 @@ if [ -z "$JAVA_OPTIONS" ] ; then
 fi
 
 $JAVA $JAVA_OPTIONS \
-    -cp config/:lib/*:alien4cloud-ui-${project.version}.war \
+    -cp config/:alien4cloud-ui-${project.version}.war \
     org.springframework.boot.loader.WarLauncher \
     "$@"
