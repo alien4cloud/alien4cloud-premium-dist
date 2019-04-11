6a1c57b71f1d8536dc8a0038c78c28ef25c4f544
EmmanuelDuru <alienclo@fcrack5.fastconnect.fr>
2019-04-11 15:53:25 +0200

    Upgrade ES 5

--- src/main/assembly/scripts/alien4cloud-ssl.bat
+++ src/main/assembly/scripts/alien4cloud-ssl.bat
@@ -10,6 +10,7 @@ if "%JAVA%" =="" (
 if "%JAVA_OPTIONS%" =="" (
   @set JAVA_OPTIONS=-server -showversion -XX:+AggressiveOpts -Xmx2g -Xms2g -XX:+HeapDumpOnOutOfMemoryError -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+DisableExplicitGC
 )
+@set JAVA_OPTIONS=%JAVA_OPTIONS% -Dlog4j.configurationFile=log4j2.xml
 
 cd "%~dp0"
 "%JAVA%" %JAVA_OPTIONS% -cp config/;alien4cloud-premium-ui-${project.version}-ssl.war org.springframework.boot.loader.WarLauncher %@
